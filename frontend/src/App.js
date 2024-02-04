9a8df28ddb0e1dda061da544316a14cfea76a61c
Pinky Huang <huangpin@oregonstate.edu>
2024-02-04 15:50:42 -0500

    Footer added, Header adjusted.
    - Basic Footer template added.
    - Header style adjusted.
    
    TODO
    - Landing page header background image
    - Landing page slogan
    - Welcome message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Landing from "./pages/Landing";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
           <Routes>
             <Route path='/' element={<Landing />} />
           </Routes>
+
+          <div id="footer">
+            <Footer />
+          </div>
         </BrowserRouter>
       </div>
     </>
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="app-footer">
+                <div className="credits">
+                    Icon designed by Freepik / Images from Unplash
+                </div>
+                <div className="copyright">
+                    © 2024 Goldstein, Hsiung, Huang, & Quach, All Rights Reserved.
+                </div>
+            </div>
+        </footer>
+    )
+}
