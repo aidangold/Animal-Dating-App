bbfa7e6b3585767bc184580d51e31ad3f8fc7817
aidangold <goldstai@oregonstate.edu>
2024-02-12 16:31:09 -0500

    added basic like button

--- frontend/src/components/Photocards.js
+++ frontend/src/components/Photocards.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import data from '../sample/pets.json';
 import { lightBlue } from '@mui/material/colors';
 import PetsRoundedIcon from '@mui/icons-material/PetsRounded';
@@ -5,9 +6,21 @@ import WcRoundedIcon from '@mui/icons-material/WcRounded';
 import TodayRoundedIcon from '@mui/icons-material/TodayRounded';
 import MonitorWeightRoundedIcon from '@mui/icons-material/MonitorWeightRounded';
 import EventAvailableRoundedIcon from '@mui/icons-material/EventAvailableRounded';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import './photocards.css';
 
 export default function Photocard() {
+    const [likedPets, setLikedPets] = useState([]);
+
+    const toggleLike = (petID) => {
+        if (likedPets.includes(petID)) {
+            setLikedPets(likedPets.filter(id => id !== petID));
+        } else {
+            setLikedPets([...likedPets, petID]);
+        }
+    };
+
     const petdata = data.pets.map((pet) => (
         <div className="photocard" key={pet.petID}>
             <img
@@ -17,21 +30,29 @@ export default function Photocard() {
             />
             <h2>{pet.petName}</h2>
             <ul>
-                <li><PetsRoundedIcon sx={{ fontSize: 16, color: lightBlue[900]}} /></li>
+                <li><PetsRoundedIcon sx={{ fontSize: 16, color: lightBlue[900] }} /></li>
                 <li>{pet.petType}</li>
-                <li><WcRoundedIcon sx={{ fontSize: 16, color: lightBlue[900]}} /></li>
+                <li><WcRoundedIcon sx={{ fontSize: 16, color: lightBlue[900] }} /></li>
                 <li>{pet.petSex}</li>
-                <li><TodayRoundedIcon sx={{ fontSize: 16, color: lightBlue[900]}} /></li>
+                <li><TodayRoundedIcon sx={{ fontSize: 16, color: lightBlue[900] }} /></li>
                 <li>{pet.petAge}</li>
-                <li><MonitorWeightRoundedIcon sx={{ fontSize: 16, color: lightBlue[900]}} /></li>
+                <li><MonitorWeightRoundedIcon sx={{ fontSize: 16, color: lightBlue[900] }} /></li>
                 <li>{pet.petWeight}</li>
-                <li><EventAvailableRoundedIcon sx={{ fontSize: 16, color: lightBlue[900]}} /></li>
+                <li><EventAvailableRoundedIcon sx={{ fontSize: 16, color: lightBlue[900] }} /></li>
                 <li>{pet.addedDate}</li>
             </ul>
             <h3>{pet.petAvailability}</h3>
+
+            <button className="like-button" onClick={() => toggleLike(pet.petID)}>
+                {likedPets.includes(pet.petID) ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
+            </button>
+            
         </div>
-    ))
+    ));
+
     return (
         <div className="photocards">{petdata}</div>
-    )
+    );
 }
+
+
