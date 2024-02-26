0bc541a6ec8a8c80b2d574d14cfab47557e47cbc
ricqosu <quachr@oregonstate.edu>
2024-02-25 23:17:12 -0800

    Add endpoint to retrieve all adoption requests (for admin)

--- backend/routes/adoption_requests.py
+++ backend/routes/adoption_requests.py
@@ -53,7 +53,53 @@ def get_adoption_requests_by_user_id():
 
 @adoption_requests_blueprint.route('/adoption_requests', methods=['GET'])
 def get_adoption_requests():
-  pass
+  try:
+    adoption_requests_data = []
+
+    query = text('''
+      SELECT 
+        pets.*,
+        EXTRACT(YEAR FROM AGE(NOW(), pets.pet_birthday)) AS pet_age,
+        users.user_first_name,
+        users.user_last_name,
+        users.user_email
+      FROM 
+        adoption_requests
+      INNER JOIN 
+        pets on adoption_requests.pet_id = pets.pet_id
+      INNER JOIN 
+        users on adoption_requests.user_id = users.user_id
+                 ''')
+    
+    result = db.session.execute(query)
+
+    for row in result:
+      pet_data = {
+        'petID': row.pet_id,
+        'petName': row.pet_name,
+        'petAge': row.pet_age,
+        'petWeight': row.pet_weight,
+        'petType': row.pet_type,
+        'petSex': row.pet_sex,
+        'petBreed': row.pet_breed,
+        'petBirthday': row.pet_birthday,
+        'goodWithAnimals': row.good_with_animals,
+        'goodWithChildren': row.good_with_children,
+        'mustBeLeashed': row.must_be_leashed,
+        'petAvailability': row.pet_availability,
+        'petPicture': f'https://{BUCKET}.s3.{REGION}.amazonaws.com/{row.pet_picture}',
+        'addedData': row.added_date,
+        'petDescription': row.pet_description,
+        'userFirstName': row.user_first_name,
+        'userLastName': row.user_last_name,
+        'userEmail': row.user_email
+      }
+      adoption_requests_data.append(pet_data)
+
+    return jsonify(adoption_requests_data)
+  except Exception as e:
+    return jsonify({'error': f'Error retrieving adoption request data for user: {str(e)}'}), 500
+
 
 
 
