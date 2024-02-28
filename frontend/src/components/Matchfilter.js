85cf61a9c92312dcfef53a6ce70ccf18d3da9c6b
Pinky Huang <huangpin@oregonstate.edu>
2024-02-28 12:58:46 -0500

    Filter state change elevated to Matching component

--- frontend/src/components/Matchfilter.js
+++ frontend/src/components/Matchfilter.js
@@ -9,48 +9,8 @@ import { useState } from "react";
 import './matchfilter.css';
 
 
-export default function Matchfilter() {
+export default function Matchfilter({ isFilter, filterClick }) {
     const [isOpen, setIsOpen] = useState(false);
-
-    const [isFilter, setIsFilter] = useState({
-        0: [],    // type
-        1: [],    // sex
-        2: [],    // age
-        3: []     // weight 
-    });
-
-    const filterClick = (index, selected) => {
-        if (selected === 'any') {
-            // if any is selected, chip is displayed active if less than max num of states
-            if (isFilter[index].length > 0) {
-                setIsFilter({...isFilter, [index]: []});
-            }
-        }
-        else {
-            // add selected to states
-            if (!isFilter[index].includes(selected)) {
-                setIsFilter({
-                    ...isFilter,
-                    [index]: [...isFilter[index], selected]
-                });
-            }
-            // deselect
-            else {
-                if (isFilter[index].length == 1) {
-                    setIsFilter({
-                        ...isFilter,
-                        [index]: []
-                    });
-                }
-                else {
-                    setIsFilter({
-                        ...isFilter,
-                        [index]: isFilter[index].filter(a => a !== selected)
-                    });
-                }      
-            }
-        }
-    }
 
     // Resizes body of content when filter menu is open
     if (isOpen) {
--- frontend/src/pages/Matching.js
+++ frontend/src/pages/Matching.js
@@ -1,13 +1,58 @@
+import { useState } from "react";
 import Matchfilter from '../components/Matchfilter';
 import Photocards from '../components/Photocards';
 import './matching.css';
 
 export default function Matching() {
+    
+    const [isFilter, setIsFilter] = useState({
+        0: [],    // type
+        1: [],    // sex
+        2: [],    // age
+        3: []     // weight 
+    });
+
+    function filterClick(index, selected) {
+        if (selected === 'any') {
+            // if any is selected, chip is displayed active if less than max num of states
+            if (isFilter[index].length > 0) {
+                setIsFilter({...isFilter, [index]: []});
+            }
+        }
+        else {
+            // add selected to states
+            if (!isFilter[index].includes(selected)) {
+                setIsFilter({
+                    ...isFilter,
+                    [index]: [...isFilter[index], selected]
+                });
+            }
+            // deselect
+            else {
+                if (isFilter[index].length == 1) {
+                    setIsFilter({
+                        ...isFilter,
+                        [index]: []
+                    });
+                }
+                else {
+                    setIsFilter({
+                        ...isFilter,
+                        [index]: isFilter[index].filter(a => a !== selected)
+                    });
+                }      
+            }
+        }
+    }
+
     return (
         <>
             <div className="filter-button">
-                <Matchfilter />
+                <Matchfilter 
+                    isFilter={isFilter}
+                    filterClick={filterClick} />
             </div>
+            
             <div className="matching-main">
                 <Photocards />
             </div>
