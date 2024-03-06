b461899a42a32d79c46e522058b8e48ce0fb7d10
Pinky Huang <huangpin@oregonstate.edu>
2024-03-06 18:15:54 -0500

    Create mostly done
    - needs to be tested

--- frontend/src/pages/AddPet.js
+++ frontend/src/pages/AddPet.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+import { uploadFile } from 'react-s3';
 import './addpet.css';
+import '../config';
 
 // !!! Validate user is logged in and has admin role
 
-// https://react.dev/reference/react-dom/components/select#reading-the-select-box-value-when-submitting-a-form
+// configure your s3 bucket values as see fit in a config.js file in frontend root
+// tutorial on how to upload file to aws s3 bucket in React:
+// https://www.mohammadfaisal.dev/blog/how-to-upload-files-to-aws-s3-in-react
+const config = {
+    bucketName: S3_BUCKET,
+    region: REGION,
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+}
 
+// https://react.dev/reference/react-dom/components/select#reading-the-select-box-value-when-submitting-a-form
 export default function AddPetForm() {
+    // For uploading image to AWS S3:
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [imgData, setImgData] = useState([]);
+
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
+    }
+
     const handleSubmit = (e) => {
         // Prevent the browser from reloading the page
         e.preventDefault();
@@ -18,8 +32,20 @@ export default function AddPetForm() {
         const form = e.target;
         const formData = new FormData(form);
 
+        // upload to s3 bucket
+        uploadFile(selectedFile, config)
+            .then((data) => {
+                console.log(data);
+                setImgData(data);   // save response data
+            })
+            .catch(err => console.error(err));
+
+        // get img location url & append to formData
+        const imgUrl = imgData.location;
+        formData.append("petPicture", imgUrl);
+
         // You can pass formData as a fetch body directly:
-        // fetch('/some-api', { method: form.method, body: formData });
+        fetch('https://animaldatingapp-backend-nzjce52oiq-ue.a.run.app/pets', { method: form.method, body: formData });
 
         // Or you can get an array of name-value pairs.
         console.log([...formData.entries()]);
@@ -28,7 +54,7 @@ export default function AddPetForm() {
     return (
         <div id='crud-main'>
             <h2>Add a Pet</h2>
-            <form className="pet-form" method="post" autoComplete="off" onSubmit={handleSubmit}>
+            <form className="pet-form" method="POST" autoComplete="off" onSubmit={handleSubmit}>
                 <label>Name </label>
                 <input type="text" name="petName" />
                 
@@ -70,6 +96,13 @@ export default function AddPetForm() {
                     <option value="Adopted">Adopted</option>
                     <option value="Unavailable">Unavailable</option>
                 </select>
+
+                <label>Description</label>
+                <textarea name="petDescription"></textarea>
+
+                <label>Image Upload</label>
+                <input type="file" onChange={handleFileInput}/>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
             </form>
         </div>
     )
